03a47c6cebdf7ee85c99bb08214059d74525f515
stigl <realstigl@gmail.com>
2024-05-10 18:55:20 +0200

    Try to separate blender extension(11)

--- build.cmd
+++ build.cmd
@@ -1,17 +1,17 @@
 set CC0= clang++ -Ofast -c
-set CC1= clang++ -Ofast -shared bin\o\types.o
+set CC1= clang++ -Ofast -shared bin\o\types.obj
 
 mkdir bin
 mkdir bin\o bin\assets bin\scripts
 
-%CC0% net.cc -o bin\o\net.o
-%CC0% file.cc -o bin\o\file.o
-%CC0% types.cc -o bin\o\types.o
-%CC0% logging.cc -o bin\o\logging.o
-%CC0% game\saving.cc -o bin\o\saving.o
+%CC0% net.cc -o bin\o\net.obj
+%CC0% file.cc -o bin\o\file.obj
+%CC0% types.cc -o bin\o\types.obj
+%CC0% logging.cc -o bin\o\logging.obj
+%CC0% game\saving.cc -o bin\o\saving.obj
 
-%CC1% game\mscript.cc -o bin\scripts\main.so
-%CC1% game\physics.cc -o bin\scripts\physics.so
-%CC1% game\scene.cc bin\o\logging.o -o bin\scripts\scene.so
-%CC1% game\movement.cc -o bin\scripts\movement.so
-%CC1% game\collision.cc -o bin\scripts\collision.so
+%CC1% game\mscript.cc -o bin\scripts\main.dll
+%CC1% game\physics.cc -o bin\scripts\physics.dll
+%CC1% game\scene.cc bin\o\logging.o -o bin\scripts\scene.dll
+%CC1% game\movement.cc -o bin\scripts\movement.dll
+%CC1% game\collision.cc -o bin\scripts\collision.dll
