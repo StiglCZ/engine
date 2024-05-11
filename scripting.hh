d57b6252af5627f8613254b4f897fe31d603bbc7
stigl <realstigl@gmail.com>
2024-05-11 09:54:48 +0200

    Try to fix native.cc for windows(2)

--- scripting.hh
+++ scripting.hh
@@ -3,14 +3,17 @@
 
 extern void *loadScript(char *file);
 extern void closeScript(void *script);
-
-#include <dlfcn.h>
 extern void scriptErr(int source);
+extern void closeAllScripts();
+template <typename T> T getFunction(void *script, char *funcName);
+#ifdef __unix__
+#include <dlfcn.h>
+
 template <typename T>
 T getFunction(void* script, char* funcName) {
     T func = reinterpret_cast<T>(dlsym(script, funcName));
     if(!func){scriptErr(1);}
     return func;
 }
-extern void closeAllScripts();
+#endif
 #endif
