617cbaab5f78ae30345bf107a19c9107f066cef2
stigl <realstigl@gmail.com>
2024-05-11 08:21:25 +0200

    Add M_PI to math.hh

--- math.hh
+++ math.hh
@@ -1,6 +1,10 @@
 #ifndef MATH
 #define MATH
 #include <cmath>
+#ifdef _WIN32
+#define M_PI 3.141592653589793
+#endif
+
 typedef float fx;
 template <typename T> inline T max(T a, T b) { return (a > b) ? a : b; }
 template <typename T> inline T min(T a, T b) { return (a < b) ? a : b; }
