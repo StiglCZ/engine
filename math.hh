b7c7f52ccd2d3c36d783ac231e0ab9b1d7f3a138
stigl <realstigl@gmail.com>
2024-08-08 12:03:28 +0200

    Fix spacing in math.hh

--- math.hh
+++ math.hh
@@ -6,5 +6,5 @@ template <typename T> inline T min(T a, T b) { return (a < b) ? a : b; }
 typedef float fx;
 constexpr fx M_PID180 = (M_PI / 180.0);
 inline fx deg(fx src) { return src / M_PID180; }
-inline fx rad(fx src){ return src * M_PID180; }
+inline fx rad(fx src) { return src * M_PID180; }
 #endif
