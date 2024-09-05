f0a61569cca93c615362795ce1f69e9e1d4632a9
stigl <realstigl@gmail.com>
2024-09-05 20:51:07 +0200

    Remove matrixes file from line counter

--- utilities/linecount.sh
+++ utilities/linecount.sh
@@ -1,6 +1,6 @@
 cat -s -n \
     *.cc *.hh *.md \
-    Makefile matrixes \
+    Makefile \
     game/* utilities/*.* \
     utilities/blender_ext/Makefile utilities/blender_ext/extension_src/*  \
     | wc -l | xargs -I {} echo "There are {} lines of code in the engine!"
